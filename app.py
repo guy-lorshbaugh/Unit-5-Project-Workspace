50c63f87c00c937da27f11d2c76ac46d2f483198
guy-lorshbaugh <67803858+guy-lorshbaugh@users.noreply.github.com>
2020-09-18 12:20:53 -0700

    Updated virtual environment, started coding

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import (Flask, url_for, render_template)
+
+import models
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hello, Flask!"
+def index():
+    journal = models.Entry.select().limit(5)
+    return render_template(url_for('index.html'))
 
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
-app.run()
-# app.run(debug=True, host='0.0.0.0', port='80')
+if __name__ == '__main__':
+    models.initialize()
+    journal= models.Entry
+    if journal.select().where(journal.id == 1).exists():
+        pass
+    else:
+        models.Entry.create(
+            content="Welcome to the Learning Journal!"
+        )
+    app.run(debug=True, host='127.0.0.1', port=80)
--- models.py
+++ models.py
@@ -1,6 +1,21 @@
 import datetime
 
-from flask.ext.bcrypt import generate_password_hash
-from flask.ext.login import UserMixin
-from peewwee import *
+# from flask_bcrypt import generate_password_hash
+# from flask_login import UserMixin
+from peewee import *
+
+DATABASE = SqliteDatabase('journal.db')
+
+class Entry(Model):
+    date = DateTimeField(default=datetime.datetime.now)
+    content = TextField()
+
+    class Meta:
+        database = DATABASE
+        order_by = ('-date',)
+
+def initialize():
+    DATABASE.connect()
+    DATABASE.create_tables([Entry], safe=True)
+    DATABASE.close()
 
